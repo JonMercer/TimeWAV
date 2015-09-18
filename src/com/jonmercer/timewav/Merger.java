753d7183d71cb1a3ba1049ab034e0cc11332a35e
JonMercer <mercer.jon@gmail.com>
2015-09-18 08:47:35 -0700

    CHECKPOINT the app is workingga . TODO: refactor code, create unit tests

--- src/com/jonmercer/timewav/Merger.java
+++ src/com/jonmercer/timewav/Merger.java
@@ -18,6 +18,9 @@ public class Merger {
     private String inputFolder;
     private String outputFolder;
 
+    private int week = 0;
+    private int year = 999;
+
     public Merger(String inputFolder, String outputFolder) {
 
         this.inputFolder = inputFolder;
@@ -26,68 +29,45 @@ public class Merger {
 
     public void merge(ArrayList<String> nameOfFilesToMerge) {
 
+
         if (nameOfFilesToMerge.size() == 1) {
-            writeWAV(nameOfFilesToMerge.get(0));
+            week = week + 1;
+            try {
+                AudioInputStream onlyWav = AudioSystem.getAudioInputStream(new File(inputFolder + "/" + nameOfFilesToMerge.get(0)));
+                writeWav(onlyWav, week, year);
+            } catch (UnsupportedAudioFileException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             return;
-        }
+        } else if (nameOfFilesToMerge.size() > 1) {
+            week = week + 1;
+            try {
+                AudioInputStream currentWav = null;
+                currentWav = AudioSystem.getAudioInputStream(new File(inputFolder + "/" + nameOfFilesToMerge.get(0)));
 
-        try {
-            AudioInputStream clip1 = AudioSystem.getAudioInputStream(new File(inputFolder + "/" + nameOfFilesToMerge.get(0)));
+                for (int i = 1; i < nameOfFilesToMerge.size(); i++) {
+                    AudioInputStream toAppend = AudioSystem.getAudioInputStream(new File(inputFolder + "/" + nameOfFilesToMerge.get(i)));
+                    currentWav =
+                            new AudioInputStream(
+                                    new SequenceInputStream(currentWav, toAppend),
+                                    currentWav.getFormat(),
+                                    currentWav.getFrameLength() + toAppend.getFrameLength());
+                }
 
-            for (int i = 1; i < nameOfFilesToMerge.size(); i++) {
-                AudioInputStream clip2 = AudioSystem.getAudioInputStream(new File(inputFolder + "/" + nameOfFilesToMerge.get(i)));
+
+                writeWav(currentWav, week, year);
+
+
+            } catch (UnsupportedAudioFileException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-        //grab second audio
-        //merge both
-
-        //write it out
-
-
-    }
-
-    private void writeWAV(String s) {
-    }
-
-
-    public void merge(ArrayList<String> fileNames, int foo) {
-        AudioInputStream currentWav = null;
-        for (int i = 0; i < fileNames.size(); i++) {
-            if(i == 0) {
-                currentWav = AudioSystem.getAudioInputStream(new File(inputFolder + "/" + INTRO));
-            } else {
-                currentWav = appendWav(currentWav, file.getName());
-            }
-        }
-
-        writeWav(currentWav, 9999, 9999);
-
-    }
-
-    private AudioInputStream appendWav(AudioInputStream currentWav, String appendName) {
-        try{
-
-            AudioInputStream toAppend = AudioSystem.getAudioInputStream(new File(inputFolder+"/"+appendName));
-            AudioInputStream appendedFiles =
-                    new AudioInputStream(
-                            new SequenceInputStream(currentWav, toAppend),
-                            currentWav.getFormat(),
-                            currentWav.getFrameLength() + toAppend.getFrameLength());
-
-            return appendedFiles;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //Note, I'm not catching this null on the return.
-        return null;
     }
 
     private void writeWav(AudioInputStream wav, int week, int year) {
@@ -107,6 +87,61 @@ public class Merger {
             e.printStackTrace();
         }
     }
+
+//    private void writeWAV(String s) {
+//    }
+//
+//
+//    public void merge(ArrayList<String> fileNames, int foo) {
+//        AudioInputStream currentWav = null;
+//        for (int i = 0; i < fileNames.size(); i++) {
+//            if(i == 0) {
+//                currentWav = AudioSystem.getAudioInputStream(new File(inputFolder + "/" + INTRO));
+//            } else {
+//                currentWav = appendWav(currentWav, file.getName());
+//            }
+//        }
+//
+//        writeWav(currentWav, 9999, 9999);
+//
+//    }
+//
+//    private AudioInputStream appendWav(AudioInputStream currentWav, String appendName) {
+//        try{
+//
+//            AudioInputStream toAppend = AudioSystem.getAudioInputStream(new File(inputFolder+"/"+appendName));
+//            AudioInputStream appendedFiles =
+//                    new AudioInputStream(
+//                            new SequenceInputStream(currentWav, toAppend),
+//                            currentWav.getFormat(),
+//                            currentWav.getFrameLength() + toAppend.getFrameLength());
+//
+//            return appendedFiles;
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        //Note, I'm not catching this null on the return.
+//        return null;
+//    }
+//
+//    private void writeWav(AudioInputStream wav, int week, int year) {
+//        try {
+//            if(week < 10) {
+//                //Prepend a 0 infront of the number
+//                AudioSystem.write(wav,
+//                        AudioFileFormat.Type.WAVE,
+//                        new File(outputFolder+"/"+year+"-0"+week+".wav"));
+//            } else {
+//                AudioSystem.write(wav,
+//                        AudioFileFormat.Type.WAVE,
+//                        new File(outputFolder+"/"+year+"-"+week+".wav"));
+//            }
+//        } catch (Exception e) {
+//            System.out.println("Error in writeWav");
+//            e.printStackTrace();
+//        }
+//    }
 
 
 
