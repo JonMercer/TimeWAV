24671cd22c473c7514c2cb07cb5c2b309794f3a3
JonMercer <mercer.jon@gmail.com>
2015-09-24 19:44:09 -0700

    added audio files. Testing will be trickier than I thought

---
+++ src/com/jonmercer/timewav/test/MergerTest.java
@@ -0,0 +1,23 @@
+package com.jonmercer.timewav.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MergerTest {
+
+    @Test
+    public void testMergeAllTheWeeks() throws Exception {
+
+    }
+
+    @Test
+    public void testMergeListOfFiles() throws Exception {
+
+    }
+
+    @Test
+    public void testWriteWav() throws Exception {
+
+    }
+}
